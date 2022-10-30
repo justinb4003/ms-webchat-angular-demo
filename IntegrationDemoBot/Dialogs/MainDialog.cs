d1ee3e23e1678589902f87fc9607b1c070be9dc2
Justin Buist <justin.buist@gmail.com>
2022-10-29 20:21:15 -0400

    CoreBot template simplified; added basic data send

---
+++ IntegrationDemoBot/Dialogs/MainDialog.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with CoreBot .NET Template version v4.17.1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CoreBot.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly ILogger _logger;
+
+        // Dependency injection uses this constructor to instantiate MainDialog
+        public MainDialog(ILogger<MainDialog> logger)
+            : base(nameof(MainDialog))
+        {
+            _logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            var waterfallSteps = new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+                FinalStepAsync,
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<ResourceResponse> SendUserData(ITurnContext ctx, CancellationToken cancellationToken, UserData data)
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+            };
+            string jsonData = JsonConvert.SerializeObject(data, jsonSerializerSettings);
+                        
+            Activity act = new Activity
+            {
+                Type = ActivityTypes.Event,
+                Name = "dataUpdate",
+                Value = jsonData
+            };
+            return await ctx.SendActivityAsync(act, cancellationToken);
+
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Use the text provided in FinalStepAsync or the default if it is the first time.
+            var messageText = stepContext.Options?.ToString() ?? "What is your name?";
+            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var fullName = stepContext.Result.ToString();
+            var p = new UserData
+            {
+                PersonId = Guid.Parse(stepContext.Context.Activity.From.Id),
+                PersonName = fullName
+            };
+            await SendUserData(stepContext.Context, cancellationToken, p);
+            var message = $"Nice to meet you, {fullName}";
+            var greetMessaage = MessageFactory.Text(message, message, InputHints.IgnoringInput);
+            await stepContext.Context.SendActivityAsync(greetMessaage, cancellationToken);
+            return await stepContext.NextAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Restart the main dialog with a different message the second time around
+            var promptMessage = "What else can I do for you?";
+            return await stepContext.ReplaceDialogAsync(InitialDialogId, promptMessage, cancellationToken);
+        }
+    }
+}
---
+++ IntegrationDemoBot/Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreBot
+{
+    public partial class UserData
+    {
+        public Guid PersonId { get; set; }
+        public string PersonName { get; set; }
+        public string Address { get; set; }
+    }
+}
