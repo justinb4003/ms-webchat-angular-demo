0fbba3dce03d2e7cbe8bcfafdccfebca39e89a65
Justin Buist <justin.buist@gmail.com>
2022-10-30 19:29:06 -0400

    Comment key sections

--- IntegrationDemoBot/Dialogs/MainDialog.cs
+++ IntegrationDemoBot/Dialogs/MainDialog.cs
@@ -28,6 +28,9 @@ namespace CoreBot.Dialogs
         {
             _logger = logger;
 
+            /* Create a Waterfall dialog that asks for a name
+             * and an addresss then restarts the process
+             */
             AddDialog(new TextPrompt(nameof(TextPrompt)));
             var waterfallSteps = new WaterfallStep[]
             {
@@ -44,6 +47,13 @@ namespace CoreBot.Dialogs
             InitialDialogId = nameof(WaterfallDialog);
         }
 
+        /* Here's our method for emitting data back to the webchat container.
+         * It will come across as an Activity of type 'event' and named
+         * 'dataUpdate'.
+         * THe handling of this is established in the createChatbotControl
+         * method where it hands it off to processIncomingData() that checks
+         * to see if it is this particular event
+         */
         private async Task<ResourceResponse> SendUserData(ITurnContext ctx, CancellationToken cancellationToken, UserData data)
         {
             var jsonSerializerSettings = new JsonSerializerSettings
@@ -67,7 +77,7 @@ namespace CoreBot.Dialogs
 
         private async Task<DialogTurnResult> PromptNameAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            // Use the text provided in FinalStepAsync or the default if it is the first time.
+            // Use the text provided in ResetDialogAsync or ask for name on initial entry
             var messageText = stepContext.Options?.ToString() ?? "What is your name?";
             var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
             return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
@@ -75,6 +85,12 @@ namespace CoreBot.Dialogs
 
         private async Task<DialogTurnResult> HandleNameAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
+            /* Gather the response as a String and assign it to a neww UserData object
+             * which we emit via SendUserData. By keeping properties we don't want to
+             * adjust in the UI left as null and setting the JSON serializer to
+             * ignore those values, we won't confuse the UI into trying to blank out a
+             * populated value.
+             */
             var fullName = stepContext.Result.ToString();
             var p = new UserData
             {
@@ -89,14 +105,14 @@ namespace CoreBot.Dialogs
         
         private async Task<DialogTurnResult> PromptAddressAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            // Use the text provided in FinalStepAsync or the default if it is the first time.
-            var messageText = stepContext.Options?.ToString() ?? "What is your address?";
+            var messageText = "What is your address?";
             var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
             return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
         }
 
         private async Task<DialogTurnResult> HandleAddressAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
+            // As before but now we only define the Address property
             var res = stepContext.Result.ToString();
             var p = new UserData
             {
